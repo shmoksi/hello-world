149cc6cfee2e596e94b67c9331d8c813737e8e54
Oksana Dmytruk <dmytruk.oksi@gmail.com>
2016-06-07 00:16:13 +0300

    changes3

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var routes = require('./routes/index');
 
 var app = express();
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 server.listen(server_port, server_ip_address, function () {
