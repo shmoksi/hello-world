536b26382d5c73dac78ed6a1b365a0b62875aaa7
Oksana Dmytruk <dmytruk.oksi@gmail.com>
2016-06-07 00:10:36 +0300

    changes2

--- app.js
+++ app.js
@@ -24,13 +24,11 @@ var routes = require('./routes/index');
 
 var app = express();
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
-// var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-// var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-server.listen(app.get('port'), app.get('ip'), function () {
-  console.log( "Listening on " + app.get('ip') + ", server_port " + app.get('port') )
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + port )
 });
 
 // view engine setup
