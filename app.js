808f34e3c64deaaf9fcce0d7a498a844b2872e2b
Oksana Dmytruk <dmytruk.oksi@gmail.com>
2016-06-24 10:10:10 +0300

    configurate db

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
   process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
   process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
   process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-  process.env.OPENSHIFT_APP_NAME;
+  'users';
 }
 
 var mongojs = require('mongojs');
