cc65e442301f1d4cf8f9ea8473e6aafc8087c696
Oksana Dmytruk <dmytruk.oksi@gmail.com>
2016-07-18 11:19:40 +0300

    change project

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const sass = require('gulp-sass');
 gulp.task( 'sass', () => {
     gulp.src( 'scss/*.scss' )
         .pipe( sass().on( 'error', sass.logError ) )
-        .pipe( gulp.dest( 'public/' ) );
+        .pipe( gulp.dest( 'public/stylesheets' ) );
 });
 
 // gulp.task('default', ['es6', 'build']);
