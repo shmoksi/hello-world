c8d29820d3ef5dc61762ae963ba411e6554daeb4
Oksana Dmytruk <dmytruk.oksi@gmail.com>
2016-07-18 11:48:18 +0300

    add mongodb

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var titlesCollection = require('./controller');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var newTitle = new titlesCollection();
+    newTitle.title = 'Oksi';
+
+    titlesCollection.createTitle(newTitle, function (err, user) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(user);
+            res.render('index', { title: user.title });
+        }
+    });
 });
 
 module.exports = router;
