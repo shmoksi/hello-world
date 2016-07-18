e2f418159375afd535d525562e719f8e63bf6ba0
Oksana Dmytruk <dmytruk.oksi@gmail.com>
2016-07-18 10:38:46 +0300

    add sass files

--- app.js
+++ app.js
@@ -10,15 +10,19 @@ var app = express();
 // app.set('ip', process.env.IP || '127.0.0.1');
 
 var port = process.env.PORT || 8080;
-var ip = process.env.IP || '127.0.0.1';
 
-http.createServer(app).listen(port, ip, function(){
+http.createServer(app).listen(port, function(){
   console.log('Express server listening on port ' + port);
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.engine('html', require('ejs').renderFile);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
+
+console.log(path.join(__dirname, 'public'));
+
 
 app.get('/', function(req, res, next) {
 	fs.readFile('db/hello.txt', function(err, data){
---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require('gulp');
+const path = require('path');
+const sass = require('gulp-sass');
+
+gulp.task( 'sass', () => {
+    gulp.src( 'scss/*.scss' )
+        .pipe( sass().on( 'error', sass.logError ) )
+        .pipe( gulp.dest( 'public/' ) );
+});
+
+// gulp.task('default', ['es6', 'build']);
